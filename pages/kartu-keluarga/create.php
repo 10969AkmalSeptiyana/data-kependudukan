651f80e298ffa7462102394504983da41e649c9c
10969AkmalSeptiyana <takaakmal10969@gmail.com>
2022-08-21 10:51:21 +0700

    add dropdown load

--- pages/kartu-keluarga/create.php
+++ pages/kartu-keluarga/create.php
@@ -33,7 +33,7 @@
     <th>ID Kepala Keluarga</th>
     <td>:</td>
     <td>
-      <select class="form-control selectlive" name="id_kepala_keluarga" required>
+      <select class="form-control selectlive" id="kepala_keluarga" name="id_kepala_keluarga" required>
         <option value="" selected disabled>- pilih -</option>
         <?php foreach ($data_warga as $warga) : ?>
         <option value="<?php echo $warga['id_warga'] ?>">
@@ -50,27 +50,27 @@
   <tr>
     <th width="20%">Alamat</th>
     <td width="1%">:</td>
-    <td><textarea class="form-control" name="alamat_keluarga"></textarea></td>
+    <td><textarea class="form-control" name="alamat_keluarga" id="alamat_keluarga"></textarea></td>
   </tr>
   <tr>
     <th>Desa/Kelurahan</th>
     <td>:</td>
-    <td><input type="text" class="form-control" name="desa_kelurahan_keluarga" required></td>
+    <td><input type="text" class="form-control" name="desa_kelurahan_keluarga" id="desa_kelurahan_keluarga" required></td>
   </tr>
   <tr>
     <th>Kecamatan</th>
     <td>:</td>
-    <td><input type="text" class="form-control" name="kecamatan_keluarga" required></td>
+    <td><input type="text" class="form-control" name="kecamatan_keluarga" id="kecamatan_keluarga" required></td>
   </tr>
   <tr>
     <th>Kabupaten/Kota</th>
     <td>:</td>
-    <td><input type="text" class="form-control" name="kabupaten_kota_keluarga" required></td>
+    <td><input type="text" class="form-control" name="kabupaten_kota_keluarga" id="kabupaten_kota_keluarga" required></td>
   </tr>
   <tr>
     <th>Provinsi</th>
     <td>:</td>
-    <td><input type="text" class="form-control" name="provinsi_keluarga" required></td>
+    <td><input type="text" class="form-control" name="provinsi_keluarga" id="provinsi_keluarga" required></td>
   </tr>
   <tr>
     <th>Negara</th>
@@ -80,17 +80,17 @@
   <tr>
     <th>RT</th>
     <td>:</td>
-    <td><input type="text" class="form-control" name="rt_keluarga" required></td>
+    <td><input type="text" class="form-control" name="rt_keluarga" id="rt_keluarga" required></td>
   </tr>
   <tr>
     <th>RW</th>
     <td>:</td>
-    <td><input type="text" class="form-control" name="rw_keluarga" required></td>
+    <td><input type="text" class="form-control" name="rw_keluarga" id="rw_keluarga" required></td>
   </tr>
   <tr>
     <th>Kode Pos</th>
     <td>:</td>
-    <td><input type="text" class="form-control" name="kode_pos_keluarga"></td>
+    <td><input type="text" class="form-control" name="kode_pos_keluarga" id="kode_pos_keluarga" ></td>
   </tr>
 </table>
 
@@ -101,3 +101,31 @@
 <?php include('../_partials/dashboard-footer.php'); ?>
 <?php include('../_partials/dashboard-logout.php'); ?>
 <?php include('../_partials/dashboard-bottom.php') ?>
+
+<script>
+  $(document).ready(function () {
+  // code to get all records from table via select box
+    $("#kepala_keluarga").change(function () {
+      var id = $(this).find(":selected").val();
+      var dataString = "wargaid=" + id;
+      $.ajax({
+        url: "../../pages/kartu-keluarga/get-keluarga.php",
+        dataType: "json",
+        data: dataString,
+        cache: false,
+        success: function (dataWarga) {
+          if (dataWarga) {
+            $("#alamat_keluarga").val(dataWarga.alamat_warga);
+            $("#desa_kelurahan_keluarga").val(dataWarga.desa_kelurahan_warga);
+            $("#kecamatan_keluarga").val(dataWarga.kecamatan_warga);
+            $("#kabupaten_kota_keluarga").val(dataWarga.kabupaten_kota_warga);
+            $("#provinsi_keluarga").val(dataWarga.provinsi_warga);
+            $("#rt_keluarga").val(dataWarga.rt_warga);
+            $("#rw_keluarga").val(dataWarga.rw_warga);
+            $("#kode_pos_keluarga").val(dataWarga.kode_pos_warga);
+          }
+        },
+      });
+    });
+  });
+</script>
